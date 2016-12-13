9e17cbdd79e1d0401cdb84c26a60b29e87928460
mikeadossi <mikeadossi@gmail.com>
2016-12-13 15:43:49 -0800

    add routing and query code

--- database.js
+++ database.js
@@ -4,9 +4,11 @@ const connectionString = `postgres://${process.env.USER}@localhost:5432/list`
 const db = pgp(connectionString);
 
 const insertTodo = 'INSERT INTO LIST (task) VALUES ($1) RETURNING task'
+const listAllTodoItems = 'SELECT * FROM list'
 
 const Todo = {
-  insert: todo => db.oneOrNone( insertTodo, [todo] )
+  insert: todo => db.oneOrNone( insertTodo, [todo] ),
+  findAll: () => db.any(listAllTodoItems)
 }
 
 module.exports = {Todo}
--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,18 @@
 const express = require('express')
 const router = express.Router()
-const Todo = require('../database').Todo
+const {Todo} = require('../database')
 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' })
 })
 
 router.post('/list', function(req, res) {
-  const task = req.body.task
-  console.log('Task', task)
-  Todo.insert(task)
-    .then(task => res.render('index', {task}) )
+  const {task} = req.body
+  Todo.insert(task).then(() =>
+    Todo.findAll().then(results =>
+      res.render('index', {results})
+    )
+  )
 })
 
 module.exports = router
