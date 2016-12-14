0a9d2b0a5b833525a352adf2d0ee513ed5d9b929
Greg <kousagi1012@yahoo.com>
2016-12-14 14:18:32 -0800

    set up mlab database, connect server.js to database

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const {MongoClient} = require('mongodb')
 const app = express()
 
-app.listen(3000, function() {
-  console.log('listening on 3000')
+MongoClient.connect('mongodb://<dbuser>:<dbpassword>@ds133378.mlab.com:33378/subsequent-turtle', (err, database) => {
+  if (err) return console.log(err)
+  db = database
+  app.listen(3000, () => {
+    console.log('listening on 3000')
+  })
 })
+
+
 
 app.use(bodyParser.urlencoded({extended: true}))
 // app.get('/', (req, res) => {
