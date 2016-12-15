8fbe6115cf199ad366fef7cad36f4b281be73257
Greg <kousagi1012@yahoo.com>
2016-12-14 16:11:51 -0800

    input will save to database

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser')
 const {MongoClient} = require('mongodb')
 const app = express()
 
-MongoClient.connect('mongodb://<dbuser>:<dbpassword>@ds133378.mlab.com:33378/subsequent-turtle', (err, database) => {
+MongoClient.connect('mongodb://turtle:asdf@ds133378.mlab.com:33378/subsequent-turtle', (err, database) => {
   if (err) return console.log(err)
   db = database
   app.listen(3000, () => {
@@ -11,17 +11,18 @@ MongoClient.connect('mongodb://<dbuser>:<dbpassword>@ds133378.mlab.com:33378/sub
   })
 })
 
-
-
 app.use(bodyParser.urlencoded({extended: true}))
-// app.get('/', (req, res) => {
-//   res.send('hello world')
-// })
+
+app.post('/todoList', (req, res) => {
+  db.collection('todoCollection').save(req.body, (err, result) => {
+    if (err) return console.log(err)
+    console.log('saved to database')
+    res.redirect('/')
+  })
+})
+
+
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
-})
-
-app.post('/todoList', (req, res) => {
-  console.log(req.body)
 })
